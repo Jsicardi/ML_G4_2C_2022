851070caec8367eaaf9610a58592d222ccd06be2
JSicardi <jsicardi@itba.edu.ar>
2022-08-10 11:56:11 -0300

    Plot generation and curated data

--- EjObligatorio1/main.py
+++ EjObligatorio1/main.py
@@ -1,9 +1,67 @@
 import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-xls = pd.ExcelFile(r"dataset.xls") # use r before absolute file path 
+columns = ['Grasas_sat','Alcohol','Calorías','Sexo']
 
-sheetX = xls.parse(0) #2 is the sheet number+1 thus if the file has only 1 sheet write 0 in paranthesis
+def boxplot(dataset, x):
+    sns.set(rc={'figure.figsize':(5,5)})
+    ax=sns.boxplot(x=x, data=dataset)
+    plt.savefig("boxplot_{0}.png".format(x))
+    plt.close()
 
-var1 = sheetX['Sexo']
+def histogram(dataset,x,bins):
+    sns.histplot(dataset[x], bins=bins)
+    plt.savefig('hist_{0}.png'.format(x))
+    plt.close()
 
-print(var1[1]) #1 is the row number...
+def scaterplot(dataset,x,y):
+    sns.scatterplot(x=x, hue=y, data=dataset)
+    plt.savefig('scat_{0}.png'.format(x))
+    plt.close()
+
+def pairplot(dataset,Hue):
+    sns.pairplot(dataset,hue=Hue,height=2)
+    plt.savefig('graf_pares.png')
+    plt.close()
+       
+def joinpoint(dataset,x,y):
+    sns.jointplot(x=x, y=y, data=dataset)
+    plt.savefig('joinpoint.png')
+    plt.close()
+
+def fill_dataset(dataset):
+    for column_name in columns[:-1]:
+        column_set = np.array(dataset[column_name])
+        values_set = column_set[(column_set != 999.99)]
+        avg = np.average(values_set)
+        for (index,value) in enumerate(column_set):
+            if(value == 999.99):
+                dataset[column_name][index] = avg
+    return dataset    
+
+def __main__():
+
+    xls = pd.ExcelFile("dataset.xls") 
+
+    dataset = xls.parse(0) #first sheet
+
+    dataset = fill_dataset(dataset)
+
+    for column_name in columns[:-1]:
+        boxplot(dataset,column_name)
+
+    for column_name in columns:
+        histogram(dataset,column_name,7)
+    
+    for column_name in columns[:-1]:
+        scaterplot(dataset,column_name,'Sexo')
+    
+    pairplot(dataset,'Sexo')
+    
+    for column_name in columns[:-1]:
+        joinpoint(dataset,column_name,'Sexo')
+    
+if __name__ == "__main__":
+    __main__()
