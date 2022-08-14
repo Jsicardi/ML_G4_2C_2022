c04ef7c577db2e5b9a43bbf0d5bf99358b0cccb1
JSicardi <jsicardi@itba.edu.ar>
2022-08-14 11:50:27 -0300

    Added double boxplots and removed unused plots

--- EjObligatorio1/main.py
+++ EjObligatorio1/main.py
@@ -5,32 +5,6 @@ import matplotlib.pyplot as plt
 
 columns = ['Grasas_sat','Alcohol','Calorías','Sexo']
 
-def boxplot(dataset, x):
-    sns.set(rc={'figure.figsize':(5,5)})
-    ax=sns.boxplot(x=x, data=dataset)
-    plt.savefig("boxplot_{0}.png".format(x))
-    plt.close()
-
-def histogram(dataset,x,bins):
-    sns.histplot(dataset[x], bins=bins)
-    plt.savefig('hist_{0}.png'.format(x))
-    plt.close()
-
-def scaterplot(dataset,x,y):
-    sns.scatterplot(x=x, hue=y, data=dataset)
-    plt.savefig('scat_{0}.png'.format(x))
-    plt.close()
-
-def pairplot(dataset,Hue):
-    sns.pairplot(dataset,hue=Hue,height=2)
-    plt.savefig('graf_pares.png')
-    plt.close()
-       
-def joinpoint(dataset,x,y):
-    sns.jointplot(x=x, y=y, data=dataset)
-    plt.savefig('joinpoint.png')
-    plt.close()
-
 def fill_dataset(dataset):
     for column_name in columns[:-1]:
         column_set = np.array(dataset[column_name])
@@ -39,7 +13,41 @@ def fill_dataset(dataset):
         for (index,value) in enumerate(column_set):
             if(value == 999.99):
                 dataset[column_name][index] = avg
-    return dataset    
+    return dataset 
+
+def boxplot(dataset, x):
+    sns.set(rc={'figure.figsize':(7,7)})
+    ax=sns.boxplot(x=x, data=dataset)
+    plt.savefig("boxplot_{0}.png".format(x))
+    plt.close()
+
+def doubleBoxplot(dataset,x,y):
+    sns.set(rc={'figure.figsize':(7,7)})
+    ax=sns.boxplot(x=x,y=y,data=dataset)
+    ax.set_xticklabels(ax.get_xticklabels())    
+    plt.savefig("boxplot_{0}_{1}.png".format(x,y))
+    plt.close()
+
+def histogram(dataset,x,bins):
+    sns.histplot(dataset[x], bins=bins)
+    plt.savefig('hist_{0}.png'.format(x))
+    plt.close()
+
+def change_sex_format(dataset):
+    mod_dataset = dataset
+    for (index,value) in enumerate(np.array(dataset['Sexo'])):
+        mod_dataset['Sexo'][index] = (0 if value == 'M' else 1)
+    return mod_dataset
+
+def scatterplot(dataset,x,y):
+    sns.scatterplot(data=dataset,x=x,y=y)
+    plt.savefig('scat_{0}_{1}.png'.format(x,y))
+    plt.close()
+
+def scatterplot_with_hue(dataset,x,y,hue):
+    sns.scatterplot(data=dataset,x=x,y=y,hue=hue)
+    plt.savefig('scat_{0}_{1}.png'.format(x,y))
+    plt.close()
 
 def __main__():
 
@@ -47,21 +55,37 @@ def __main__():
 
     dataset = xls.parse(0) #first sheet
 
+    #replace empty values with avg
     dataset = fill_dataset(dataset)
 
+    #generate single variable boxplot
     for column_name in columns[:-1]:
         boxplot(dataset,column_name)
 
-    for column_name in columns:
-        histogram(dataset,column_name,7)
+    #generate histogram for Sex
+    histogram(dataset,'Sexo',2)
     
+    #generate scatterplot with Sex
     for column_name in columns[:-1]:
-        scaterplot(dataset,column_name,'Sexo')
+        scatterplot(dataset,column_name,'Sexo')
+
+    #generate boxplots
+    for column_name in columns[:-1]:    
+        doubleBoxplot(dataset,'Sexo',column_name)
     
-    pairplot(dataset,'Sexo')
-    
-    for column_name in columns[:-1]:
-        joinpoint(dataset,column_name,'Sexo')
+    #classify calories by categories
+    categories = []
+    for cal_value in dataset['Calorías']:
+        if cal_value < 1100:
+            categories.append('CATE 1')
+        elif cal_value < 1700:
+            categories.append('CATE 2')
+        else:
+            categories.append('CATE 3')
+
+    #generate scatterplot by categories
+    scatterplot_with_hue(dataset,'Calorías','Alcohol',categories)
+
     
 if __name__ == "__main__":
     __main__()
