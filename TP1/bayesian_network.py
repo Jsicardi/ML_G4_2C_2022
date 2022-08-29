439babee1fcb2ee51360a343acf04e9b0b242769
JSicardi <jsicardi@itba.edu.ar>
2022-08-29 13:32:35 -0300

    Added more cases to bayesian network

--- TP1/bayesian_network.py
+++ TP1/bayesian_network.py
@@ -19,33 +19,54 @@ class BayesianNetwork:
                 root_absolute_probs.append(1)
                 root_conditional_probs.append(properties.last_probabilities[test_value[2]-1])
             
-            #check if gre and gpa attributes are missing
+            #case gre and gpa attributes are missing
+            middle_conditional_probs = [[],[]]
+            first_middle_values = []
+            second_middle_values = []
+            
+            if(test_value[0] == -1):
+                for probs in properties.middle_probabilities[0]:
+                    middle_conditional_probs[0].append(probs)
+                first_middle_values = [0,1]
+            else:
+                for probs in properties.middle_probabilities[0]:
+                    middle_conditional_probs[0].append([1,1])
+                first_middle_values = [test_value[0]]
+            if(test_value[1] == -1):
+                for probs in properties.middle_probabilities[1]:
+                    middle_conditional_probs[1].append(probs)
+                second_middle_values = [0,1]
+            else:
+                for probs in properties.middle_probabilities[1]:
+                    middle_conditional_probs[1].append([1,1])
+                second_middle_values = [test_value[1]]
+            
             prob = 0
-            if(test_value[0] == -1 and test_value[1] == -1):
-                for first_middle_value in [0,1]:
-                    for second_middle_value in [0,1]:
+            if(test_value[2] != -1):
+                for first_middle_value in first_middle_values:
+                    for second_middle_value in second_middle_values:
                         sum_prob = 0
                         for (root_idx,root_prob) in enumerate(properties.root_probabilities):
                             prod_prob = 1
                             middle_values = [first_middle_value,second_middle_value]
-                            for (prob_idx,probs) in enumerate(properties.middle_probabilities):
-                                print(probs[root_idx][middle_values[prob_idx]])
+                            for (prob_idx,probs) in enumerate(middle_conditional_probs):
                                 prod_prob*=(probs[root_idx][middle_values[prob_idx]])
                             
                             prod_prob *= root_prob
                             sum_prob+=prod_prob
                         prob+=(properties.last_probabilities[test_value[2]-1][first_middle_value][second_middle_value][class_idx] * sum_prob)
-            
-            elif(test_value[0] != -1 and test_value[1] != -1):
+            else:
                 for (cond_idx,conditional_probs) in enumerate(root_conditional_probs):
                     prob+=(conditional_probs[test_value[0]][test_value[1]][class_idx]) * root_absolute_probs[cond_idx]
 
             class_probs.append(prob)
+
         return class_probs
         
     def classify(self,properties:NetworkProperties):
         predictions = []
         probabilities = []
+        
         for test_value in properties.test_values:
             probabilities.append([])
             class_probs = self.get_class_probs(properties,test_value)
